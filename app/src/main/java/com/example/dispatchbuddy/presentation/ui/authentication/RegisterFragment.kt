164b6b63d8ee40d3040ea3bbed96962e668700d0
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-07-15 10:26:01 +0100

    connected the welcome, sign in and sign up screens

--- app/src/main/java/com/example/dispatchbuddy/presentation/ui/authentication/RegisterFragment.kt
+++ app/src/main/java/com/example/dispatchbuddy/presentation/ui/authentication/RegisterFragment.kt
@@ -1,10 +1,11 @@
-package com.example.dispatchbuddy.presentation.ui
+package com.example.dispatchbuddy.presentation.ui.authentication
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.dispatchbuddy.R
 import com.example.dispatchbuddy.databinding.FragmentRegisterBinding
 
@@ -20,4 +21,20 @@ class RegisterFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            fragmentRegisterSignUpBtn.setOnClickListener {
+                findNavController().navigate(R.id.loginFragment)
+            }
+            fragmentRegisterHaveAccountTv.setOnClickListener {
+                findNavController().navigate(R.id.loginFragment)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
