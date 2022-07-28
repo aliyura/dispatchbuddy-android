032ef8aedea837293f722564a1ecdc11e6d515d4
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-07-28 13:53:21 +0100

    implemented image picker

--- app/src/main/java/com/example/dispatchbuddy/common/Constants.kt
+++ app/src/main/java/com/example/dispatchbuddy/common/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val PERMISSION_ID = 5
     const val REQUEST_CHECK_SETTINGS = 100
     const val BASE_URL = "https://dispatchbuddy.net/api/"
+    const val GALLERY = 100
+    const val GALLERY_PERMISSION_CODE = 101
 }
