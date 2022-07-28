03a0774decb07233a6ded17f1365321600b3dd58
Ifeyinwa Unoka <unokaifeyinwaruth@gmail.com>
2022-07-28 12:40:17 +0100

    registration endpoint implementation

--- app/src/main/java/com/example/dispatchbuddy/common/Constants.kt
+++ app/src/main/java/com/example/dispatchbuddy/common/Constants.kt
@@ -1,8 +1,7 @@
 package com.example.dispatchbuddy.common
 
 object Constants {
-    val LOCATION_PERMISSION_REQUEST = 1
     const val PERMISSION_ID = 5
     const val REQUEST_CHECK_SETTINGS = 100
-    const val BASE_URL = "https://dispatchbuddy.net/api/"
+    const val BASE_URL = "https://api.dispatchbuddy.net/api/"
 }
---
+++ app/src/main/java/com/example/dispatchbuddy/data/remote/dto/models/VerifyUser.kt
@@ -0,0 +1,6 @@
+package com.example.dispatchbuddy.data.remote.dto.models
+
+data class VerifyUser(
+    val otp: String,
+    val username: String
+)
