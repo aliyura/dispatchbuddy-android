9c89e105155e69c5aaac1f011dfed6cda6ec52a1
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-08-02 12:07:29 +0100

    wrote a util function for handling logout functionality

--- app/src/main/java/com/example/dispatchbuddy/common/Utils.kt
+++ app/src/main/java/com/example/dispatchbuddy/common/Utils.kt
@@ -1,12 +1,17 @@
 package com.example.dispatchbuddy.common
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.ContentResolver
+import android.content.Context
+import android.content.res.Resources
 import android.net.Uri
 import android.provider.OpenableColumns
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.dispatchbuddy.databinding.LogoutDialogLayoutBinding
 import java.util.*
 
 fun getDaysAgo(daysAgo: Int): Date {
@@ -24,4 +29,24 @@ fun ContentResolver.getFileName(uri: Uri): String {
         name = cursor.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
     }
     return name
+}
+
+fun showLogOutDialog(context: Context, binding: LogoutDialogLayoutBinding, resources: Resources, logOutFunction: () -> Unit): AlertDialog {
+    val builder = AlertDialog.Builder(context)
+    builder.setView(binding.root)
+    builder.setCancelable(false)
+    val dialog = builder.create()
+    val width = (resources.displayMetrics.widthPixels * 0.80).toInt()
+    val height = (resources.displayMetrics.heightPixels * 0.35).toInt()
+    dialog!!.window?.setLayout(width, height)
+
+    binding.logoutDialogYesTextView.setOnClickListener {
+        logOutFunction.invoke()
+        Toast.makeText(context,"Logout successful", Toast.LENGTH_SHORT).show()
+        dialog.dismiss()
+    }
+    binding.logoutDialogNoTextView.setOnClickListener {
+        dialog.dismiss()
+    }
+    return dialog
 }
