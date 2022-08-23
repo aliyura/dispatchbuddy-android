3ffc7daacc68de76d0fd2c86b18b1b1953978047
Ifeyinwa Unoka <unokaifeyinwaruth@gmail.com>
2022-08-23 21:13:15 +0100

    add api key to constant file

--- app/src/main/java/com/example/dispatchbuddy/presentation/ui/user_dashboard/HomeFragment.kt
+++ app/src/main/java/com/example/dispatchbuddy/presentation/ui/user_dashboard/HomeFragment.kt
@@ -13,6 +13,7 @@ import com.example.dispatchbuddy.BuildConfig
 import com.example.dispatchbuddy.R
 import com.example.dispatchbuddy.common.Constants.PICKUP_REQUEST_CODE
 import com.example.dispatchbuddy.common.Constants.DELIVERY_REQUEST_CODE
+import com.example.dispatchbuddy.common.Constants.GOOGLE_PLACES_API_KEY
 import com.example.dispatchbuddy.common.ViewExtensions.showShortSnackBar
 import com.example.dispatchbuddy.common.preferences.Preferences
 import com.example.dispatchbuddy.common.validation.FieldValidationTracker
@@ -62,7 +63,7 @@ class HomeFragment : Fragment() {
         }
 
        // Initialize places sdk
-        Places.initialize(requireContext(), BuildConfig.GOOGLE_PLACES_API_KEY)
+        Places.initialize(requireContext(), GOOGLE_PLACES_API_KEY)
     }
 
     private fun onSearchCalled(requestCode: Int) {
